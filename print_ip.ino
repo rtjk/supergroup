2add864adf5f033b9d08789f3688ac4ae51ae8a0
rtjk <47841774+rtjk@users.noreply.github.com>
2023-05-04 01:02:45 +0200

    Add files via upload

---
+++ print_ip.ino
@@ -0,0 +1,38 @@
+////////////////////////
+///////SUPERGROUP///////
+////////////////////////
+
+// ESP32
+// USB
+
+#include <WiFi.h>
+
+const char* ssid = "test";
+const char* password = "test1234567!";
+
+String hostname = "esp32";
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+}
+
+void loop() {
+  
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  WiFi.setHostname(hostname.c_str());
+  //wifi_station_set_hostname( hostname.c_str() );
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
